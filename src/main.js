111e99c45157183f5b74d62a70472bdb732811d1
Жанна Нарсия <z.narsija@superjob.ru>
2019-02-20 23:13:50 +0300

    module1-task2 Правки по код-ревью

--- src/main.js
+++ src/main.js
@@ -40,37 +40,30 @@ const filters = [
 
 const filtersParentElement = document.querySelector(`.main__filter`);
 const cardsParentElement = document.querySelector(`.board__tasks`);
-let isCheckedFilter = false;
-
-const setChecked = (filterElement) => {
-  filterElement.querySelector(`input`).setAttribute(`checked`, `true`);
-  isCheckedFilter = true;
-};
 
 const showFilters = function (filtersArray) {
   const fragment = document.createDocumentFragment();
   filtersArray.forEach(function (item) {
     const templateFilterMarkup = document.querySelector(`#filter-template`).content;
     const filterElement = templateFilterMarkup.cloneNode(true);
+    const filterInputElement = filterElement.querySelector(`.filter__input`);
     const filterLabelElement = filterElement.querySelector(`.filter__label`);
     const filterCountElement = filterElement.querySelector(`.filter__count`);
+    const itemName = item.name.toLowerCase();
 
-    filterElement.querySelector(`input`).setAttribute(`id`, `filter__${item.name.toLowerCase()}`);
+    filterInputElement.checked = item.name === `ALL`;
+    filterInputElement.setAttribute(`id`, `filter__${itemName}`);
 
-    if (item.count === 0) {
-      filterElement.querySelector(`input`).setAttribute(`disabled`, `true`);
-    } else if (!isCheckedFilter) {
-      setChecked(filterElement);
-    }
-
-    filterLabelElement.setAttribute(`for`, `filter__${item.name.toLowerCase()}`);
+    filterLabelElement.setAttribute(`for`, `filter__${itemName}`);
     filterLabelElement.firstChild.textContent = `${item.name} `;
     filterLabelElement.addEventListener(`click`, function () {
       cardsParentElement.textContent = ``;
       showCards(getRandomNumber());
     });
-    filterCountElement.className = `filter__${item.name.toLowerCase()}-count`;
+
+    filterCountElement.className = `filter__${itemName}-count`;
     filterCountElement.textContent = `${item.count}`;
+
     fragment.appendChild(filterElement);
   });
   filtersParentElement.appendChild(fragment);
