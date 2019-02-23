4a59b9758b06a1bcbe698ea5ae9558ed4e45b0ca
Жанна Нарсия <z.narsija@superjob.ru>
2019-02-23 20:32:05 +0300

    module2-task1 Замена функций IIFE на подключение модулей

--- src/filter.js
+++ src/filter.js
@@ -1,20 +1,15 @@
-'use strict';
-
-(function () {
-  window.renderFilter = function (checked = false, labelText, count) {
-    return (
-      `<input
+export default function (checked = false, labelText, count) {
+  return (
+    `<input
           type="radio"
-          id="filter__all"
+          id="filter__${labelText}"
           class="filter__input visually-hidden"
           name="filter"
           ${checked ? `checked` : ``}
           ${count === 0 ? `disabled` : ``}
       />
-      <label for="filter__all" class="filter__label"> ${labelText}
+      <label for="filter__${labelText}" class="filter__label"> ${labelText}
         <span class="filter__count filter__all-count">${count}</span>
       </label>`
-    );
-  };
-})();
-
+  );
+}
